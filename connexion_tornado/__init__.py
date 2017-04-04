bfe8f34d649ef005b0d6019aa8519cd091461ac0
Rafael Caricio <rafael.caricio@zalando.de>
2017-04-04 20:19:19 +0200

    Draw first sketch of API

---
+++ connexion_tornado/__init__.py
@@ -0,0 +1,9 @@
+
+
+class TornadoApp:
+    def add_api(self, yaml=None):
+        pass
+
+    def listen(self, port):
+        pass
+
---
+++ test.py
@@ -0,0 +1,36 @@
+import tornado.web
+import tornado.ioloop
+
+from connexion_tornado import TornadoApp
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+
+app = TornadoApp()
+app.add_api(yaml="""
+swagger: "2.0"
+info:
+  title: "Hello World API"
+  version: "1.0"
+paths:
+  /:
+    get:
+      summary: Hello to everyone
+      description: Generates a greeting message.
+      operationId: test.MainHandler#get
+      produces:
+        - text/plain;
+      responses:
+        200:
+          description: greeting response
+          schema:
+            type: string
+          examples:
+            "text/plain": "Hello John"
+""")
+app.listen(8888)
+
+tornado.ioloop.IOLoop.current().start()
